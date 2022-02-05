a14dd2f4c5d716bd32d39c71f7f1b21df950057f
Daniel Adamski <dra2103@gmail.com>
2022-02-05 14:44:46 -0800

    commit

---
+++ Java_fundamentals/First_Java_Program/First.java
@@ -0,0 +1,8 @@
+public class First{
+    public static void main(String[] args){
+        System.out.println("My name is Daniel Adamski");
+        System.out.println("I am 34 years old");
+        System.out.println("My hometown is San Diego, CA");
+
+    }
+}
