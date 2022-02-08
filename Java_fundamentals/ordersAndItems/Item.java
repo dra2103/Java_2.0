e9f8238ded40b8d403696a34017df0582b860452
Daniel Adamski <dra2103@gmail.com>
2022-02-07 21:59:01 -0800

    commit

---
+++ Java_fundamentals/ordersAndItems/Item.java
@@ -0,0 +1,5 @@
+public class Item {
+    // Member Variables
+    public String name;
+    public double price;
+}
---
+++ Java_fundamentals/ordersAndItems/Order.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Order {
+    // Memebr Variables
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item>items = new ArrayList<Item>();
+
+}
