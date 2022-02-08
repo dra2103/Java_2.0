d1949a0685e1a997bdd54ffaa9b2dd82b8e7fbbf
Daniel Adamski <dra2103@gmail.com>
2022-02-07 22:08:52 -0800

    commit

--- Java_fundamentals/ordersAndItems/TestOrders.java
+++ Java_fundamentals/ordersAndItems/TestOrders.java
@@ -29,25 +29,53 @@ public class TestOrders {
 
         Order order2 = new Order();
         order2.name = "Jimmy";
-        order2.total = item1.price;
-        order2.items.add(item1);
-        order.ready = true;
+
 
         Order order3 = new Order();
         order3.name = "Noah";
-        order3.total = item4.price;
-        order3.items.add(item4);
+
 
         Order order4 = new Order();
         order4.name = "Sam";
-        order4.total = item2.price*3;
-        order4.items.add(item2);
-        order4.items.add(item2);
-        order4.items.add(item2);
 
         // thought it would give me an Error of some sort, gave me 0.0
         System.out.println(order1.total);
 
+        // item1 to order 2
+        order2.items.add(item1);
+        order2.total += item1.price;
+
+        // order3 ordered cappuccino
+        order3.items.add(item4);
+        order3.total += item4.price;
+
+        //order4 added a latte
+        order4.items.add(item2);
+        order4.total += item2.price;
+
+        //Cindhuri's order ready
+        order1.ready = true;
+
+        // Sam ordered more drinks - 2 lattes
+        order4.items.add(item2);
+        order4.items.add(item2);
+        order4.total += item2.price *2;
+
+        // Jimmys order is ready
+        order2.ready = true;
+
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Ready: %s\n", order1.ready);
+        
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Total: %s\n", order2.total);
+        System.out.printf("Ready: %s\n", order2.ready);
+        
+        System.out.printf("Name: %s\n", order3.name);
+        System.out.printf("Total: %s\n", order3.total);
+        System.out.printf("Ready: %s\n", order3.ready);
+        
         System.out.printf("Name: %s\n", order4.name);
         System.out.printf("Total: %s\n", order4.total);
         System.out.printf("Ready: %s\n", order4.ready);
